349586c3154ca447eccc9e99b57984d550d33fad
Guilherme Vale <guilhermevale@Guilhermes-MacBook-Air.local>
2021-08-25 00:08:17 -0300

    Second commit: Added CSS, Tailwindcss and nodejs, todos also created

--- ProjectTest/resources/views/index.blade.php
+++ ProjectTest/resources/views/index.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.default')
 
 @section('content')
-Teste
+<section class="mb-6">
+    <div class="max-w-screen-md mx-auto">
+        <form action ="{{url('/todo')}}" method ="POST">
+            @csrf
+            <div class="flex justify-center shadow rounded">
+                <input type="text" name="title" id="title" class="w-full rounded-l px-4 py-2 outline-none">
+                <input type="submit" value="Create" class="bg-purple-500 px-4 py-2 rounded-r text-white"/>
+            </div>
+        </form>
+    </div>
+</section>
+<section>
+    <div class="max-w-screen-md mx-auto">
+        <div class="bg-white p-4 rounded-xl shadow mb-3 flex justify-between">
+            <div class="title">
+                todo teste
+            </div>
+            <form>
+                @csrf 
+                @method ('DELETE')
+                <button type="submit" class="text-red-500">
+                    DELETE
+                </button>
+            </form>
+        </div>
+    </div>
+</section>
 @endsection
--- ProjectTest/resources/views/layouts/default.blade.php
+++ ProjectTest/resources/views/layouts/default.blade.php
@@ -8,7 +8,14 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css')}}"/>
 </head>
-<body>
+<body class="bg-gray-100">
+    <header>
+        <div class="container mx-auto p-4">
+            <h1 class="text-2xl font-bold text-center">Laravel Todos</h1>
+        </div>
+    </header>
+    <main class="p-4">
     @yield('content')
+    </main>
 </body>
 </html>
